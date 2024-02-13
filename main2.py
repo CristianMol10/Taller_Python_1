25c085e226b23ea73a15d4659c5f6c7802d760b9
CristianMol10 <159323656+CristianMol10@users.noreply.github.com>
2024-02-13 20:09:49 +0000

    Ajuste main2 y Agregar main3

--- main2.py
+++ main2.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-matriz1 = np.array([[1, 1, 1],[2, 2, 2]])
-
-matriz2 = np.array([[1, 1, 1],[2, 2, 2]])
+matriz1 = np.array([[1, 1, 1], [2, 2, 2]])
+matriz2 = np.array([[9, 9, 9], [9, 9, 9]])
 
 suma_matrices = matriz1 + matriz2
 resta_matrices = matriz1 - matriz2
 producto_punto = np.dot(matriz1, matriz2.T)
 producto_cruz = np.cross(matriz1, matriz2)
-division_matrices = matriz1 / matriz2
+division_matrices = matriz1.astype(float) / matriz2
 
 print("Matriz 1:")
 print(matriz1)
@@ -23,4 +22,4 @@ print(producto_punto)
 print("\nProducto cruz de matrices:")
 print(producto_cruz)
 print("\nDivisión de matrices:")
-print(division_matrices)
+print(division_matrices.round(2))
---
+++ main3.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+x = 2
+y = 2
+z = 5
+
+def recta_cilindricas(x, y, z):
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return r, theta, float(z)
+
+def recta_esfericas(x, y, z):
+  p = np.sqrt(x**2 + y**2 + z**2)
+  theta = np.arctan2(y, x)
+  phi = np.arccos(z / p)
+  return float(p), float(theta), float(phi)
+
+r_cilindrica, theta_cilindrica, z_cilindrica = recta_cilindricas(x, y, z)
+print("\nCoordenadas cilíndricas:")
+print("r =", round(r_cilindrica, 2))
+print("theta =", round(theta_cilindrica, 2))
+print("z =", round(z_cilindrica, 2))
+
+p_esferica, theta_esferica, phi_esferica = recta_esfericas(x, y, z)
+print("\nCoordenadas esfericas:")
+print("p =", round(p_esferica, 2))
+print("theta =", round(theta_esferica, 2))
+print("phi =", round(theta_esferica, 2),)
