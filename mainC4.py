86adc711a95b65e26d4853a709afd296502d05f2
CristianMol10 <159323656+CristianMol10@users.noreply.github.com>
2024-02-28 11:08:42 -0500

    Actualización mainC4.py

--- mainC4.py
+++ mainC4.py
@@ -1,29 +1,33 @@
-"""
-import numpy as np
 import matplotlib.pyplot as plt
 
-x=input(print("ingrese el valor de la cordenada X"))
-y=input(print("ingrese el valor de la cordenada Y"))
-z=input(print("ingrese el valor de la cordenada Z"))
+def vector(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-vector1 = np.array([x, y, z])
 
-plt.figure(figsize=(5, 5))
-plt.title("sistema cordenado XYZ")
-plt.grid(True)
-plt.show()
-"""
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
-import numpy as np
+    ax.quiver(0, 0, 0, 1, 0, 0, color='r', arrow_length_ratio=0.1)
+    ax.quiver(0, 0, 0, 0, 1, 0, color='g', arrow_length_ratio=0.1)
+    ax.quiver(0, 0, 0, 0, 0, 1, color='b', arrow_length_ratio=0.1)
 
-fig = plt.figure()
+  
+    ax.quiver(0, 0, 0, x, y, z, color='k', arrow_length_ratio=0.5)
 
-ax1 = fig.add_subplot(111,projection='3d')
-x = (0,20)
-y = (0,20)
-z = (0,20)
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-5, 5])
+    ax.set_zlim([-5, 5])
 
-ax1.plot_wireframe(x, y, z)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-plt.show()
+    plt.show()
+
+def main():
+    print("Ingrese las coordenadas del vector:")
+    x = float(input("Componente X: "))
+    y = float(input("Componente Y: "))
+    z = float(input("Componente Z: "))
+
+    vector(x, y, z)
+
+main()
